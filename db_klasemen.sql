755b25d91eeccddb1d5a3c2ea8adfe1e0e945763
FAHMI <57124286+vahmiy@users.noreply.github.com>
2025-07-01 23:48:16 +0700

    Add files via upload

---
+++ db_klasemen.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2025 at 06:47 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_klasemen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `klub`
+--
+
+CREATE TABLE `klub` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `kota` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `klub`
+--
+
+INSERT INTO `klub` (`id`, `nama`, `kota`) VALUES
+(4, 'Persib', 'Bandung'),
+(5, 'Arema', 'Jawa Timur'),
+(6, 'Persija', 'Jakarta');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pertandingan`
+--
+
+CREATE TABLE `pertandingan` (
+  `id` int(11) NOT NULL,
+  `klub1_id` int(11) DEFAULT NULL,
+  `klub2_id` int(11) DEFAULT NULL,
+  `skor_klub1` int(11) DEFAULT NULL,
+  `skor_klub2` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pertandingan`
+--
+
+INSERT INTO `pertandingan` (`id`, `klub1_id`, `klub2_id`, `skor_klub1`, `skor_klub2`) VALUES
+(3, 4, 5, 2, 1),
+(4, 4, 6, 2, 0),
+(5, 5, 6, 1, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `klub`
+--
+ALTER TABLE `klub`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama` (`nama`);
+
+--
+-- Indexes for table `pertandingan`
+--
+ALTER TABLE `pertandingan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `klub`
+--
+ALTER TABLE `klub`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pertandingan`
+--
+ALTER TABLE `pertandingan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
